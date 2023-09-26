4beb403df621dbba5d0fa38ff83785de3ee380e9
JeanQrocha <jean.qrocha96@gmail.com>
2023-09-26 20:56:44 -0300

    atualizando pastas

--- DEVCLUB/ProjetoForEach/products.js
+++ DEVCLUB/ProjetoForEach/products.js
@@ -1,8 +1,8 @@
 const menuOptions = [
-    { name: 'X-Salada', price: 30, vegan: false, src: "/DEVCLUB/DevClub/Projeto-forEach/assets/xsalada.jpeg"},
-    { name: 'X-Bacon', price: 35, vegan: false, src: "/DEVCLUB/DevClub/Projeto-forEach/assets/xbacon.png"},
-    { name: 'X-Bacon Egg', price: 40, vegan: false, src: "/DEVCLUB/DevClub/Projeto-forEach/assets/xbaconegg.png"},
-    { name: 'X-Monstruoso', price: 50, vegan: false, src: "/DEVCLUB/DevClub/Projeto-forEach/assets/monstruoso.png"},
-    { name: 'X-Vegano', price: 35, vegan: true, src: "/DEVCLUB/DevClub/Projeto-forEach/assets/xvegano.png"},
-    { name: 'X-Big Vegano', price: 48, vegan: true, src: "/DEVCLUB/DevClub/Projeto-forEach/assets/veganoM.png"},
+    { name: 'X-Salada', price: 30, vegan: false, src: "/DEVCLUB/ProjetoForEach/assets/xsalada.jpeg"},
+    { name: 'X-Bacon', price: 35, vegan: false, src: "/DEVCLUB/ProjetoForEach/assets/xbacon.png"},
+    { name: 'X-Bacon Egg', price: 40, vegan: false, src: "/DEVCLUB/ProjetoForEach/assets/xbaconegg.png"},
+    { name: 'X-Monstruoso', price: 50, vegan: false, src: "/DEVCLUB/ProjetoForEach/assets/monstruoso.png"},
+    { name: 'X-Vegano', price: 35, vegan: true, src: "/DEVCLUB/ProjetoForEach/assets/xvegano.png"},
+    { name: 'X-Big Vegano', price: 48, vegan: true, src: "/DEVCLUB/ProjetoForEach/assets/veganoM.png"},
     ]
